44fc51d349ed8a283712101d5eb419ec0b49dc80
Mariana <marianaafeerreira@gmail.com>
2023-05-18 11:21:37 -0300

    wip

--- python/trabalho/q1/calculaDistanciaEntreDoisPontos.py
+++
@@ -1,3 +0,0 @@
-def calculaDistancia(x1, x2, y1, y2):
-    d = 0.5**((x2 - x1)**2+(y2-y2)**2)
-    return d
--- python/trabalho/q2/main.py
+++
@@ -1,5 +0,0 @@
-import calculaCustoConsumidor as m1
-
-custoFabrica = float(input("Custo de fabrica do produto:\n"))
-custoConsumidor = m1.calculaCustoConsumidor(custoFabrica)
-print("Custo ao consumidor:\n", custoConsumidor)
