107e2002b838f3571a2ea8a558d296a8b782276b
Mariana <marianaafeerreira@gmail.com>
2023-07-04 10:39:43 -0300

    wip

--- python/m2/t2/q2/calculaCustoConsumidor.py
+++
@@ -1,8 +0,0 @@
-def calculaCustoConsumidor(custoFabrica):
-    pd = custoFabrica * 0.28
-    impostos = custoFabrica * 0.45
-
-    custoConsumidor = custoFabrica + pd + impostos
-    return custoConsumidor
-
-
