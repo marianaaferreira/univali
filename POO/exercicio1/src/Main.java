da159b5818f1fa8ea77f83c60259c32195702eba
Mariana <marianaafeerreira@gmail.com>
2023-08-18 11:16:58 -0300

    wip

---
+++ POO/exercicio1/src/Main.java
@@ -0,0 +1,25 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Invoice fatura = new Invoice();
+        String numero, descricao;
+        int qtdComprada;
+        double precoItem;
+        System.out.println("Informe o número da fatura:");
+        numero = input.nextLine();
+        fatura.setNum(numero);
+        System.out.println("Informe a descrição:");
+        descricao = input.nextLine();
+        fatura.setDescricao(descricao);
+        System.out.println("Informe a qtd comprada:");
+        qtdComprada = input.nextInt();
+        fatura.setQtdComprada(qtdComprada);
+        System.out.println("Informe o preço por item:");
+        precoItem = input.nextDouble();
+        fatura.setPrecoItem(precoItem);
+        System.out.printf("O valor da fatura é %f", fatura.getInvoiceAmount(precoItem, qtdComprada));
+    }
+}
