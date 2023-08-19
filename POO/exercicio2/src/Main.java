f31ca4f34e6c3761f35bcbbca06195e2ea0fcfab
Mariana <marianaafeerreira@gmail.com>
2023-08-19 14:00:20 -0300

    wip

--- POO/exercicio2/src/Main.java
+++ POO/exercicio2/src/Main.java
@@ -1,5 +1,13 @@
+
+/*2 Crie uma classe chamada Employee que inclua três atributos — um primeiro nome (tipo String), um
+sobrenome (tipo String) e um salário mensal (double). Forneça um construtor que inicializa as três
+variáveis de instância. Forneça um método set e um get para cada variável de instância. Se o salário mensal
+não for positivo, não configure seu valor. Escreva um aplicativo para testar as capacidades da classe
+Employee. Crie dois objetos Employee e exiba o salário anual de cada objeto. Então dê a cada Employee
+um aumento de 10% e exiba novamente o salário anual de cada Employee.
+*/
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ POO/exercicio2/src/br/univali/poo/exercicio2/Employee.java
@@ -0,0 +1,30 @@
+package br.univali.poo.exercicio2;
+
+public class Employee {
+    private String nome;
+    private String sobrenome;
+    private double salario;
+}
+
+public void setNome(String nome){
+    this.nome = nome;
+}
+
+public String getNome(){
+    return nome;
+}
+public void setSobrenome(String sobrenome){
+    this.sobrenome = sobrenome;
+}
+
+public String getSobrenome(){
+    return sobrenome;
+}
+
+public void setSalario(String salario){
+    this.salario = salario;
+}
+
+public double getSalario(){
+    return salario;
+}
