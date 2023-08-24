5f1b7c50b7e51419ba03019ef4ffb6c7b96b1c65
Mariana <marianaafeerreira@gmail.com>
2023-08-24 11:52:54 -0300

    wip

--- estrutura de dados/exerciciosListaEstaticaGenerica/q3/main.cpp
+++ estrutura de dados/exerciciosListaEstaticaGenerica/q3/main.cpp
@@ -4,9 +4,27 @@
 using namespace std;
 //3. Faça uma rotina para retornar o menor e o maior elemento de uma lista ListaEstaticaGenerica.
 
+string maiorEMenorElemento(ListaEstaticaGenerica<int, 10> lista, int tam){
+    int maior=recuperaElemento(lista, 0), menor=recuperaElemento(lista, 0);
+    for(int i=1; i<tam; i++){
+        if (recuperaElemento(lista, i)>maior)
+            maior = recuperaElemento(lista, i);
+        if(recuperaElemento(lista, i)<menor)
+            menor = recuperaElemento(lista, i);
+    }
+    return "Maior: " + to_string(maior) + "\nMenor: " + to_string(menor);
+}
 
 int main(){
+    ListaEstaticaGenerica<int, 10> l;
+    int n=10;
 
+    for(int i=0; i<n; i++){
+        insere(l, i, rand()%10);
+    }
+
+    exibeLista(l);
+    cout << maiorEMenorElemento(l, n);
 
     return 0;
 }
