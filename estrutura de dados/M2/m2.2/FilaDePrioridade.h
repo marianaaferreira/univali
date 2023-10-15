55bf7f8b8fc9f45e7f69239bcd5ae5435ecf8eb6
Mariana <marianaafeerreira@gmail.com>
2023-10-15 17:05:25 -0300

    wip

--- estrutura de dados/M2/m2.2/FilaDePrioridade.h
+++ estrutura de dados/M2/m2.2/FilaDePrioridade.h
@@ -1,6 +1,8 @@
 #ifndef FILADEPRIORIDADE_H_INCLUDED
 #define FILADEPRIORIDADE_H_INCLUDED
 
+using namespace std;
+
 template<typename T>
 struct Nodo{
     T elemento;
@@ -32,12 +34,85 @@ void destroi(FilaDePrioridade<T> &fila){
         delete p;
     }
     fila.cardinalidade = 0;
-    fila.fim = NULL:
+    fila.fim = NULL;
 }
 
 template<typename T>
-void existeElemento(FilaDePrioridade<T> fila){
+bool existeElemento(FilaDePrioridade<T> fila, T elemento){
+    Nodo<T> *p = fila.inicio;
+    while(p!=NULL){
+        if(p->elemento==elemento)
+            return true;
+        p = p->proximo;
+    }
+    return false;
+}
 
+template<typename T>
+void insere(FilaDePrioridade<T> &fila, T elemento, char prioridade){
+    Nodo<T> *n = new Nodo<T>;
+
+    prioridade = toupper(prioridade);
+    if(prioridade!='B' and prioridade!='M' and prioridade!='A')
+        throw "CARACTER INVALIDO";
+
+    n->elemento = elemento;
+    n->prioridade = prioridade;
+    if(fila.inicio==NULL){
+        fila.inicio = n;
+        fila.fim = n;
+        n->anterior=NULL;
+        n->proximo=NULL;
+    }else{
+        fila.fim->proximo = n;
+        n->anterior = fila.fim;
+        n->proximo = NULL;
+        fila.fim = n;
+    }
+    fila.cardinalidade++;
+}
+
+template<typename T>
+void retira(FilaDePrioridade<T> &fila){
+    Nodo<T> *p = fila.inicio;
+    bool achou=false;
+    while(p!=NULL and not achou){
+        if(p->prioridade=='A') achou = true;
+    }
+    if(not achou)
+        while(p!=NULL or not achou){
+            if(p->prioridade=='M') achou = true;
+        }
+    if(not achou) p = fila.inicio;
+    if(p==fila.inicio){
+        fila.inicio = p->proximo;
+        p->proximo->anterior=NULL;
+    }
+    else if(p==fila.fim){
+        fila.fim = p->anterior;
+        p->anterior->proximo = NULL;
+    }
+    else{
+        p->anterior->proximo = p->proximo;
+        p->proximo->anterior = p->anterior;
+    }
+    delete p;
+    fila.cardinalidade--;
+}
+
+template<typename T>
+void mostra(FilaDePrioridade<T> fila){
+    Nodo<T> *p;
+    for(p=fila.inicio; p!=NULL; p->proximo)
+        if(p->prioridade=='A')
+            cout << p->elemento << " ";
+    for(p=fila.inicio; p!=NULL; p->proximo)
+        if(p->prioridade=='M')
+            cout << p->elemento << " ";
+    for(p=fila.inicio; p!=NULL; p->proximo)
+        if(p->prioridade=='B')
+            cout << p->elemento << " ";
+    cout << endl;
 }
 
 #endif // FILADEPRIORIDADE_H_INCLUDED
--- estrutura de dados/M2/m2.2/main.cpp
+++ estrutura de dados/M2/m2.2/main.cpp
@@ -2,9 +2,45 @@
 #include "FilaDePrioridade.h"
 
 using namespace std;
-
+/* criar uma fila, destruir uma fila,
+verificar se um elemento existe na fila, inserir um elemento na fila,retirar um elemento da fila e mostrar
+uma fila (por ordem de prioridade).*/
 int main(){
-    char prioridade;
+    char prioridade, op='1';
+    FilaDePrioridade<int> fila;
+    int elemento;
 
+    cria(fila);
+    while(op!=4){
+        cout << "Menu" << endl;
+        cout << "1. Verificar se um elemento existe na fila" << endl;
+        cout << "2. Inserir elemento" << endl;
+        cout << "3. Retirar elemento" << endl;
+        cout << "4. Mostrar fila" << endl;
+        cout << "5. Sair" << endl;
+        cin.get(op); cin.ignore();
+
+        switch(op){
+            case '1':
+                cout << "Informe o elemento: "; cin >> elemento;
+                if (existeElemento(fila, elemento))
+                    cout << "O elemento esta na fila" << endl;
+                else
+                    cout << "O elemento nao esta na fila" << endl;
+                break;
+            case '2':
+                cout << "Informe o elemento: "; cin >> elemento;
+                cout << "\nInforme a prioridade: "; cin.get(prioridade);
+                insere(fila, elemento, prioridade);
+                break;
+            case '3':
+                retira(fila);
+                break;
+            case '4':
+                mostra(fila);
+        }
+    }
+
+    destroi(fila);
     return 0;
 }
