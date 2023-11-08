ce148a38f3f087b357f163f1c1fdab9d6d169d88
DESKTOP-0CDRRID\Administrador <suporte@univali.br>
2023-11-07 21:31:27 -0300

    wip

--- poo/cinema/src/br/univali/poo/cinema/Main.java
+++ poo/cinema/src/br/univali/poo/cinema/Main.java
@@ -1,7 +1,11 @@
 package br.univali.poo.cinema;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList<Sessao> sessoes = new ArrayList<Sessao>();
+        Sessao s = new Sessao("Coraline", 90)
+        Funcionario f = new Funcionario()
     }
 }
