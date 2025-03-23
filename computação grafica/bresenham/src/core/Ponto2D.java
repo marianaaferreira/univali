ff8f6d9ef960e416a1e121dd608aeebf9d60dc6e
Mariana <111542448+marianaaferreira@users.noreply.github.com>
2025-03-23 17:47:25 -0300

    wip

---
+++ computação grafica/bresenham/src/core/Ponto2D.java
@@ -0,0 +1,13 @@
+package core;
+
+public class Ponto2D {
+    public float x;
+    public float y;
+
+    public Ponto2D(float x, float y) {
+        super();
+        this.x = x;
+        this.y = y;
+    }
+
+}
