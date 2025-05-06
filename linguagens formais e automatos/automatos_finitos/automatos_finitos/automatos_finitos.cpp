bd03f17315ba6d142be475ca3f8f0dc342b7b71f
Mariana <111542448+marianaaferreira@users.noreply.github.com>
2025-05-06 09:10:04 -0300

    add finalized algorithm version of the project without graphical interface

--- linguagens formais e automatos/automatos_finitos/automatos_finitos/automatos_finitos.cpp
+++ linguagens formais e automatos/automatos_finitos/automatos_finitos/automatos_finitos.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
 #define MAX_SENTENCAS 20
 #define QTD_CARACTERES 5
-#define COLUNA_ERRO 8
+#define COLUNA_ERRO 5
 
-bool reconhece_sentenca(int tabela[9][6], bool ef[], string sentenca) {
+bool reconhece_sentenca(int tabela[10][6], bool ef[], string sentenca) {
     char simbolo=' ';
-    int estado = 0, coluna, indice_sentenca=0; 
+    int estado = 4, coluna, indice_sentenca=0; 
 
     while (indice_sentenca < sentenca.length()) {
         simbolo = sentenca[indice_sentenca]; indice_sentenca++; 
-        simbolo = toupper(simbolo);
-        char c = 'A'; int i = 0; coluna = COLUNA_ERRO;
-        for (; c <= 'E' and i < QTD_CARACTERES+1; c++, i++)
+        //simbolo = toupper(simbolo);
+        char c = 'a'; int i = 0; coluna = COLUNA_ERRO;
+        for (; c <= 'e' and i < QTD_CARACTERES+1; c++, i++)
             if (c == simbolo) coluna = i;
         estado = tabela[estado][coluna];
         //cout << "estado: " << estado << "coluna: " << coluna  << endl; 
@@ -26,8 +27,8 @@ bool reconhece_sentenca(int tabela[9][6], bool ef[], string sentenca) {
 int separa_sentencas(string entrada, string sentencas[]) {
     int i = 0, j=0;
     string sentenca="";
-    while (i <= entrada.length()) {
-        if (entrada[i] != '+' and entrada[i] != '-' and entrada[i] != '*' and entrada[i] != '/' and entrada[i] != ' ') {
+    while (i < entrada.length()) {
+        if (entrada[i] != '+' and entrada[i] != '-' and entrada[i] != '*' and entrada[i] != '/' and not isspace(entrada[i])) {
             sentenca += entrada[i];
         }
         else{
@@ -45,18 +46,19 @@ int separa_sentencas(string entrada, string sentencas[]) {
 }
 
 int main(){
-    int tabela[9][6] = {
-        {1,8,2,3,8,8},
-        {8,4,8,8,8,8},
-        {8,8,5,3,6,8},
-        {8,8,8,3,6,8},
-        {1,8,2,3,8,8},
-        {8,8,2,3,6,8},
-        {8,8,8,8,7,8},
-        {8,8,8,8,6,8},
-        {8,8,8,8,8,8}
+    int tabela[10][6] = {
+        {6,9,5,2,7,9},
+        {9,9,5,2,7,9},
+        {9,9,9,2,7,9},
+        {9,9,9,9,7,9},
+        {8,9,1,9,9,9},
+        {9,9,1,9,9,9},
+        {9,4,9,9,9,9},
+        {9,9,9,9,3,9},
+        {9,0,9,9,9,9},
+        {9,9,9,9,9,9}
     };
-    bool ef[] = { 1,0,1,1,1,1,0,1,0 };
+    bool ef[] = { 1,1,1,1,0,0,0,0,0,0 };
     string entrada, sentenca, sentencas[MAX_SENTENCAS];
     int n_sentencas;
 
@@ -69,8 +71,17 @@ int main(){
             cout << "operador aritmetico: " << sentencas[i] << endl;
         else if (reconhece_sentenca(tabela, ef, sentencas[i]))
             cout << "sentenca valida: " << sentencas[i] << endl;
-        else cout << "ERRO: sentenca invalida: " << sentencas[i] << endl; //separar em diferentes tipos de erros
+        else if (sentencas[i][1] != 'a' and  sentencas[i][1] != 'b' and sentencas[i][1] != 'c' and sentencas[i][1] != 'd' and sentencas[i][1] != 'e')
+            cout << "ERRO: simbolos invalidos: '" << sentencas[i] << "'" << endl;
+        else
+            cout << "ERRO: sentenca invalida: '" << sentencas[i] <<"'"<< endl; 
     }
+
+    /*for (int i = 0; i < n_sentencas; i++) {
+        cout << sentencas[i] << "  indice: " << i << endl;
+    }*/
+
+    //if(reconhece_sentenca(tabela, ef, "abd")) cout << "reconhece"; 
 
     //if (reconhece_sentenca(tabela, ef, entrada)) cout << "reconhece"; else cout << "nao reconhece";
 
